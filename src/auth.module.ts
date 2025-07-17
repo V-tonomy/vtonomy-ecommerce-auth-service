483b2bee112ddb324b160c26b0dc21aa933d637c
AnChu <80581001+Chuan1404@users.noreply.github.com>
2025-07-17 15:49:16 +0700

    update auth service

--- src/auth.module.ts
+++ src/auth.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
 import { JwtModule } from '@nestjs/jwt';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { MongooseModule } from '@nestjs/mongoose';
-import { CLIENTS, CoreModule } from 'vtonomy';
+import { CLIENTS } from 'vtonomy';
 import { AUTH_HANDLER } from './core';
 import { TokenSchema } from './domain/token.schema';
 import { AuthController } from './infras/auth.transport';
@@ -10,27 +11,36 @@ import { TokenRepository } from './infras/token.repository';
 
 @Module({
   imports: [
-    CoreModule,
+    CqrsModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'jwt-secret',
       signOptions: { expiresIn: '7d' },
     }),
-    MongooseModule.forRoot(process.env.MONGODB_URL ?? 'mongodb://localhost:27017/ecommerce'),
+    MongooseModule.forRoot(
+      process.env.MONGODB_URL ?? 'mongodb://localhost:27017/ecommerce',
+    ),
     MongooseModule.forFeature([{ name: 'Token', schema: TokenSchema }]),
     ClientsModule.register([
       {
         name: CLIENTS.User_Client,
         transport: Transport.RMQ,
         options: {
-          urls: [process.env.RABBITMQ_URL ?? 'amqp://vtonomy:123456@localhost:5672'],
+          urls: [
+            process.env.RABBITMQ_URL ?? 'amqp://vtonomy:123456@localhost:5672',
+          ],
           queue: 'user_queue',
+          queueOptions: {
+            durable: false,
+          },
         },
       },
       {
         name: CLIENTS.Auth_Client,
         transport: Transport.RMQ,
         options: {
-          urls: [process.env.RABBITMQ_URL ?? 'amqp://vtonomy:123456@localhost:5672'],
+          urls: [
+            process.env.RABBITMQ_URL ?? 'amqp://vtonomy:123456@localhost:5672',
+          ],
           queue: 'auth_queue',
           queueOptions: {
             durable: true,
@@ -41,7 +51,9 @@ import { TokenRepository } from './infras/token.repository';
         name: CLIENTS.Notification_Client,
         transport: Transport.RMQ,
         options: {
-          urls: [process.env.RABBITMQ_URL ?? 'amqp://vtonomy:123456@localhost:5672'],
+          urls: [
+            process.env.RABBITMQ_URL ?? 'amqp://vtonomy:123456@localhost:5672',
+          ],
           queue: 'notification_queue',
           queueOptions: {
             durable: true,
